8a147cb62e2ae4ad7e4e18118d97139b50370716
Alfonso Aninias <168547379+Nikoroji23@users.noreply.github.com>
2025-12-17 15:48:27 +0800

    SCRIPT JS

---
+++ Alfonso Aninias/My Website/script.js
@@ -0,0 +1,19 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+const typed = new Typed('.multiple-text', {
+      strings: [' FrontEnd Developer', ' UI/UX Designer', ' Computer Engineer', 'Web Developer'],
+      typeSpeed: 80,
+      backSpeed: 80,   
+      backdelay: 1200,
+      loop: true,
+    });
